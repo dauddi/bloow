40f843b30e50db2971976c705779a8072b184129
dauddi <kuntaytc@gmail.com>
2022-02-12 04:45:23 +0300

    initial routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
+import { Navigate, Routes, Route } from 'react-router-dom'
 import logo from './assets/images/logo.svg'
-import { Dashboard, LandingPage } from './pages'
+import { Homepage, Layout, SearchPage, FavoritesPage, UploadPage, ProfilePage } from './pages'
 
 const auth = true;
 
+!auth ? <Navigate to="/welcome" /> : null;
+
 function App() {
   return (
-    auth ? <Dashboard /> : <LandingPage />
+    <Layout>
+      <Routes>
+        <Route path="/" element={ <Homepage/> } />
+        <Route path="search" element={ <SearchPage /> } />
+        <Route path="favorites" element={ <FavoritesPage /> } />
+        <Route path="upload" element={ <UploadPage /> } />
+        <Route path="profile" element={ <ProfilePage /> } />
+      </Routes>
+      <h1>Home</h1>
+    </Layout>
   )
 }
 

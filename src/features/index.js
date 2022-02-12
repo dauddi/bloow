c15f298ff33b7a69e1c4c7972afcfae3136d2187
dauddi <kuntaytc@gmail.com>
2022-02-12 04:44:26 +0300

    updated multi-export files

--- src/features/index.js
+++ src/features/index.js
@@ -1,2 +1,1 @@
-export { default as MainSection } from "./MainSection";
 export { default as SidebarMenu } from "./SidebarMenu";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,7 @@
 export { default as LandingPage } from "./LandingPage";
-export { default as Dashboard } from "./Dashboard";
+export { default as Layout } from "./Layout";
+export { default as Homepage } from "./Homepage";
+export { default as SearchPage } from "./SearchPage";
+export { default as UploadPage } from "./UploadPage";
+export { default as ProfilePage } from "./ProfilePage";
+export { default as FavoritesPage } from "./FavoritesPage";

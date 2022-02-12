d5c561bf9d55b7b10e3d753b88be2d1912da6007
dauddi <kuntaytc@gmail.com>
2022-02-12 04:47:13 +0300

    initial music player widget

--- src/widgets/MusicPlayerWidget.jsx
+++ src/widgets/MusicPlayerWidget.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MusicPlayerWidget = () => {
   return (
-    <div className="w-full absolute bottom-0 left-0 right-0 h-32 bg-gray-500" >
+    <div className="w-full absolute bottom-0 left-0 right-0 h-32 bg-purple-100 rounded-lg " >
       Music PLayer Widget
     </div>
   )

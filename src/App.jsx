845679d8a88f3c5accfb7ee317e72a020678cec1
dauddi <kuntaytc@gmail.com>
2022-02-14 07:31:49 +0300

    subscribed app component to the redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, Routes, Route } from 'react-router-dom'
 import logo from './assets/images/logo.svg'
 import { Homepage, Layout, SearchPage, FavoritesPage, UploadPage, ProfilePage } from './pages'
+
+import { useSongs } from '../lib/hooks'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToStoreSongsArray } from './redux/slices/songsSlice'
 
 const auth = true;
 
 !auth ? <Navigate to="/welcome" /> : null;
 
 function App() {
+  const dispatch = useDispatch();
+  const songs = useSongs();
+  dispatch(addToStoreSongsArray(songs))
+
   return (
     <Layout>
       <Routes>
@@ -17,7 +25,6 @@ function App() {
         <Route path="upload" element={ <UploadPage /> } />
         <Route path="profile" element={ <ProfilePage /> } />
       </Routes>
-      <h1>Home</h1>
     </Layout>
   )
 }

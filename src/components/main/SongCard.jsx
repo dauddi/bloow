6b292374d07f77f971bf84d27ebd61f9635ffa92
dauddi <kuntaytc@gmail.com>
2022-02-14 23:07:50 +0300

    refactored div to figure element

--- src/components/main/SongCard.jsx
+++ src/components/main/SongCard.jsx
@@ -15,17 +15,23 @@ const SongCard = ({ song }) => {
     }
   }, [activeSongInQueue, activeSongInQueue.isPLaying])
 
+  const handlePlay = () => {
+    if (activeSongInQueue.isPlaying) dispatch(pauseSong());
+    setTimeout(() => dispatch(playSong(song)), 100);
+  }
+
+  const handlePause = () => {
+    dispatch(pauseSong());
+  }
+
   return (
-    <div key={song.id} className="relative flex flex-col w-[20%] m-4 hover:scale-105 transition-all " >
-      <img src={song.cover} alt={`${song.title} album cover image`} />
-      <div className="absolute inset-0 text-7xl transition-all text-green-200 " >
-        { !isPlaying && <BsPlayCircleFill onClick={() => {
-            if (activeSongInQueue.isPlaying) dispatch(pauseSong());
-            setTimeout(() => dispatch(playSong(song)), 100)
-          }} className="absolute bottom-[5%] left-[5%] hover:cursor-pointer hover:text-green-300" /> } 
-        { isPlaying && <BsPauseCircleFill onClick={() => dispatch(pauseSong())} className="absolute bottom-[5%] left-[5%] hover:cursor-pointer hover:text-green-300" /> }
-      </div>
-  </div>
+    <figure key={song.id} className="relative flex flex-col min-w-[180px] min-h-[180px] ml-2 md:w-[20%] md:m-4 hover:scale-105 transition-all " > 
+      <img src={song.cover} className="rounded-md" alt={`${song.title} album cover image`} />
+      <figcaption className="absolute inset-x-0 bottom-0 h-10 bg-black bg-opacity-20 text-3xl hover:text-5xl transition-all " >
+        { !isPlaying && <BsPlayCircleFill onClick={handlePlay} className="absolute bottom-1.5 right-[5%] hover:cursor-pointer text-gray-100" /> } 
+        { isPlaying && <BsPauseCircleFill onClick={handlePause} className="absolute bottom-1.5 right-[5%] hover:cursor-pointer text-green-300" /> }
+      </figcaption>
+    </figure>
   )
 }
 

9a570465bb4a2182a7033fe6f41fc20d41585714
dauddi <kuntaytc@gmail.com>
2022-02-14 07:37:16 +0300

    minor layout changes on the sidebar menu

--- src/features/SidebarMenu.jsx
+++ src/features/SidebarMenu.jsx
@@ -15,8 +15,8 @@ const sidebarMenuItems = [
 
 const MenuItem = ({ Icon, Text }) => {
   return (
-    <div className="flex items-center justify-start p-4 hover:cursor-pointer hover:bg-purple-100 hover:rounded-md ">
-      <Icon />
+    <div className="flex items-center justify-start p-4 font-bold hover:cursor-pointer hover:bg-purple-100 hover:rounded-md hover:text-[rgb(2,0,36)] ">
+      <Icon className="text-xl" />
       <h2 className="pl-4" >{Text}</h2>
     </div>
   )
@@ -24,21 +24,23 @@ const MenuItem = ({ Icon, Text }) => {
 
 const Sidebar = () => {
   return (
-    <div className="flex-col justify-evenly text w-[25%] h-screen" >
+    <div className="flex-col justify-between text w-full h-screen" >
       <Link to="/" className="hover:cursor-pointer " >
         <Brand Logo={Logo} Title={title} />
       </Link>
 
       <div className="flex-col text-center justify-center m-10" >
         { sidebarMenuItems.map(item => (
-          <NavLink to={`${item.path}`} >
+          <NavLink key={item.path} to={`${item.path}`} >
             <MenuItem Icon={item.icon} Text={item.text}/>
           </NavLink>
-        ) ) }
+        )) }
       </div>
 
-      <div>
-        <menuItem Icon={FiSettings} Text="Settings" />
+      <div className="flex-col text-center justify-center m-10 ">
+        <NavLink to="profile" >  
+          <MenuItem Icon={FiSettings} Text="Settings" />
+        </NavLink>
       </div>
     </div>
   )

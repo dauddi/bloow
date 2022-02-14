7f6f2a58e6ccbf9dabfa2382bdd6f4fba87bd57e
dauddi <kuntaytc@gmail.com>
2022-02-14 07:36:27 +0300

    logo label color changes

--- src/components/sidebar/Brand.jsx
+++ src/components/sidebar/Brand.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Brand = ({ Logo, Title }) => {
   return (
-    <div className="flex items-center justify-start m-10 " >
+    <div className="flex items-center justify-start mx-10 " >
       <img src={Logo} width={40} height='auto' alt="bloow logo" />
-      <h2 className="font-bold text-2xl pl-3 text-purple-900 " > {Title} </h2>
+      <h2 className="font-bold text-2xl pl-3 text-orange-300 " > {Title} </h2>
     </div>
   )
 };

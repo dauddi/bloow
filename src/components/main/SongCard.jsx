02afac943ce4f6eb23cc5680ed2732d7a4c65c62
dauddi <kuntaytc@gmail.com>
2022-02-18 08:21:23 +0300

    reduced song card size

--- src/components/main/SongCard.jsx
+++ src/components/main/SongCard.jsx
@@ -25,7 +25,7 @@ const SongCard = ({ song }) => {
   }
 
   return (
-    <figure key={song.id} className="relative flex flex-col min-w-[180px] min-h-[180px] ml-2 md:w-[20%] md:m-4 hover:scale-105 transition-all " > 
+    <figure key={song.id} className="relative flex flex-col min-w-[180px] min-h-[180px] ml-2 md:w-[15%] md:m-4 hover:scale-105 transition-all " > 
       <img src={song.cover} className="rounded-md" alt={`${song.title} album cover image`} />
       <figcaption className="absolute inset-x-0 bottom-0 h-10 bg-black bg-opacity-20 text-3xl hover:text-5xl transition-all " >
         { !isPlaying && <BsPlayCircleFill onClick={handlePlay} className="absolute bottom-1.5 right-[5%] hover:cursor-pointer text-gray-100" /> } 

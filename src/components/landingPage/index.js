b61d553a9afd5b26e8dab06e130d2ca0dab02475
dauddi <kuntaytc@gmail.com>
2022-02-18 08:14:55 +0300

    updated multiexport files

---
+++ src/components/landingPage/index.js
@@ -0,0 +1,2 @@
+export { default as HeadphoneCanvas } from "./HeadphoneCanvas";
+export { default as AuthForm } from "./AuthForm";
--- src/features/index.js
+++ src/features/index.js
@@ -1,2 +1,3 @@
 export { default as SidebarMenu } from "./SidebarMenu";
 export { default as MobileNav } from "./MobileNav";
+export { default as MobileHeader } from "./MobileHeader";

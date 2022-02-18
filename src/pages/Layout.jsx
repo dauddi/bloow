f308e88ad6cfca45aaf2b91f602efa9e81538833
dauddi <kuntaytc@gmail.com>
2022-02-18 08:17:14 +0300

    added mobile app header to layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SidebarMenu, MobileNav } from '../features';
+import { SidebarMenu, MobileNav, MobileHeader } from '../features';
 import { MusicPlayerWidget } from '../widgets'
 
 import { useSelector } from 'react-redux';
@@ -9,14 +9,17 @@ const Layout = ({ children }) => {
   return (
     <div className="w-screen overflow-x-hidden" >
       <div className="w-full flex" >
-        <div className="hidden md:flex sidebar top-0 left-0 bottom-0 w-[250px] text-white " >
+        <div className="hidden md:flex md:fixed bg-indigo-900 top-0 left-0 p-4 bottom-0 w-[300px] text-white " >
           <SidebarMenu />
         </div>
-        <div className="w-full" >
+        <div className="w-full md:ml-[300px]" >
+          <div className='md:hidden' >
+            <MobileHeader />
+          </div>
           { children }
         </div>
       </div>
-      { playQueue.length && <div className="w-full fixed left-0 right-0 bottom-24 md:bottom-0 player h-[8%] md:h-[100px] bg-transparent text-slate-200">
+      { playQueue.length && <div className="w-full fixed left-0 right-0 bottom-20 md:bottom-0 h-[8%] md:h-[100px] bg-indigo-700 text-slate-200">
         <MusicPlayerWidget />
       </div>}
       <MobileNav />

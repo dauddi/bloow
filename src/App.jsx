f3941e559b558c6d8f1c9b3293c7a3927d079140
dauddi <kuntaytc@gmail.com>
2022-02-18 08:13:20 +0300

    conditional rendering to mimick authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navigate, Routes, Route } from 'react-router-dom'
-import logo from './assets/images/logo.svg'
-import { Homepage, Layout, SearchPage, FavoritesPage, UploadPage, ProfilePage } from './pages'
+import { Homepage, Layout, SearchPage, FavoritesPage, UploadPage, ProfilePage, LandingPage } from './pages'
 
 import { useSongs } from '../lib/hooks'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToStoreSongsArray } from './redux/slices/songsSlice'
 
-const auth = true;
-
-!auth ? <Navigate to="/welcome" /> : null;
-
 const App = () => {
   const dispatch = useDispatch();
+  const { isAuth } = useSelector(state => state.auth);
+  const [auth, setAuth] = useState(isAuth || false);
   const songs = useSongs();
-  dispatch(addToStoreSongsArray(songs))
+  dispatch(addToStoreSongsArray(songs));
+
+  useEffect(() => {
+    setAuth(isAuth);
+  }, [isAuth])
 
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={ <Homepage/> } />
-        <Route path="search" element={ <SearchPage /> } />
-        <Route path="favorites" element={ <FavoritesPage /> } />
-        <Route path="upload" element={ <UploadPage /> } />
-        <Route path="profile" element={ <ProfilePage /> } />
-      </Routes>
-    </Layout>
+        <Routes>
+          <Route path="/" element={ auth ? <Layout> <Homepage/> </Layout> : <LandingPage /> } />
+          <Route path="search" element={ <Layout> <SearchPage /> </Layout> } />
+          <Route path="favorites" element={ <Layout> <FavoritesPage /> </Layout> } />
+          <Route path="upload" element={ <Layout> <UploadPage /> </Layout> } />
+          <Route path="profile" element={ <Layout> <ProfilePage /> </Layout> } />
+        </Routes>
   )
 }
 

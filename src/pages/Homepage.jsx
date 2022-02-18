22af8fa8e088429d38aa34d20751f1c58164ec76
dauddi <kuntaytc@gmail.com>
2022-02-18 08:19:31 +0300

    moved mobile header to shared layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import { useSongs } from '../../lib/hooks'
 import banner from '../assets/images/banner.png'
 import SongCard from '../components/main/SongCard'
-import { BsSoundwave } from 'react-icons/bs'
-import { CgProfile } from 'react-icons/cg'
 import { useSelector } from 'react-redux'
 
 const title = "Bloow";
@@ -13,15 +11,6 @@ const Homepage = () => {
 
   return (
     <div className="w-screen m-0 p-1" >
-      <div className="flex justify-between items-center p-4 md:hidden" >
-        <div className='flex gap-2 justify-center items-center text-purple-900 text-xl hover:text-purple-900 font-extrabold' >
-          <BsSoundwave className='text-2xl' />
-          <h1>Bloow Music</h1>
-        </div>
-        <div className='flex flex-col justify-center items-center text-2xl text-purple-900 ' >
-          <CgProfile />
-        </div>
-      </div>
       <div className="w-full md:hidden">
         <img className="rounded-sm" src={banner} alt="bloow banner" />
       </div>

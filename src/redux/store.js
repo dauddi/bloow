518278b671dc4fd310260358b5edcbc6452df302
dauddi <kuntaytc@gmail.com>
2022-02-18 08:15:20 +0300

    added auth slice to the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import songsReducer from "./slices/songsSlice";
+import authReducer from "./slices/authSlice";
 
 const store = configureStore({
 	reducer: {
 		songs: songsReducer,
+		auth: authReducer,
 	},
 });
 
